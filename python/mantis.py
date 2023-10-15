5f2d019c358be37db73180b2e2ba6b5b9a2ee90c
Sjoerd Langkemper <sjoerd-github@linuxonly.nl>
2023-10-15 17:34:00 +0200

    Speed up encryption
    Use different data model

--- python/mantis.py
+++ python/mantis.py
@@ -1,111 +1,69 @@
-class Value:
-    def __init__(self, val):
-        try:
-            self.val = int.from_bytes(val, "big")
-        except TypeError:
-            assert isinstance(val, int)
-            self.val = val
+def to_nibbles(val: int):
+    assert isinstance(val, int)
+    assert 0 <= val < 1 << 64
 
-    def __int__(self):
-        return self.val
-
-    def __xor__(self, other):
-        return Value(self.val ^ other.val)
-
-    def __eq__(self, other):
-        return isinstance(other, self.__class__) and self.val == other.val
-
-    def __repr__(self):
-        swapped = int.from_bytes(self.to_bytes(), "little")
-        return f"0x{self.val:016x} ({swapped:016x})"
-
-    def ror(self, rotations):
-        rotate = self.val & ((1 << rotations) - 1)
-        val = (self.val >> rotations) | rotate << (64 - rotations)
-        return Value(val)
-
-    def __rshift__(self, shifts):
-        return Value(self.val >> shifts)
-
-    def to_bytes(self):
-        return self.val.to_bytes(8, "big")
-
-    def to_nibbles(self):
-        result = []
-        val = self.val
-        for i in range(16):
-            nibble = (val & 0xF000000000000000) >> 60
-            result.append(nibble)
-            val <<= 4
-        return result
-
-    def clone(self):
-        return Value(self.val)
-
-    def sbox(self, sbox):
-        val = self.val
-        result = 0
-
-        for i in range(16):
-            result <<= 4
-            nibble = (val & 0xF000000000000000) >> 60
-            result |= sbox[nibble]
-            val <<= 4
-
-        return Value(result)
-
-    def permutate(self, p):
-        result = 0
-        nibbles = self.to_nibbles()
-        for i in range(16):
-            result <<= 4
-            result |= nibbles[p[i]]
-        return Value(result)
-
-    def mix_columns(self):
-        def M(v):
-            return [
-                v[1] ^ v[2] ^ v[3],
-                v[0] ^ v[2] ^ v[3],
-                v[0] ^ v[1] ^ v[3],
-                v[0] ^ v[1] ^ v[2],
-            ]
-
-        rows = [
-            (self.val >> 48) & 0xFFFF,
-            (self.val >> 32) & 0xFFFF,
-            (self.val >> 16) & 0xFFFF,
-            (self.val >> 0) & 0xFFFF,
-        ]
-        rows = M(rows)
-        new_value = (rows[0] << 48) | (rows[1] << 32) | (rows[2] << 16) | rows[3]
-        return Value(new_value)
+    result = []
+    for i in range(16):
+        nibble = (val & 0xF000000000000000) >> 60
+        result.append(nibble)
+        val <<= 4
+    return result
 
 
-class Mantis:
+def permutate(val: int, p: list[int]):
+    assert isinstance(val, int)
+    assert 0 <= val < 1 << 64
+
+    result = 0
+    nibbles = to_nibbles(val)
+    for i in range(16):
+        result <<= 4
+        result |= nibbles[p[i]]
+    return result
+
+
+def to_int(val):
+    return int.from_bytes(val, "big")
+
+
+def to_bytes(val):
+    return val.to_bytes(8, "big")
+
+
+def ror(val: int, rotations: int):
+    assert isinstance(val, int)
+    assert 0 <= val < 1 << 64
+
+    rotate = val & ((1 << rotations) - 1)
+    val = (val >> rotations) | rotate << (64 - rotations)
+    return val
+
+
+class Encryption:
     rc = [
-        Value(0x13198A2E03707344),
-        Value(0xA4093822299F31D0),
-        Value(0x082EFA98EC4E6C89),
-        Value(0x452821E638D01377),
-        Value(0xBE5466CF34E90C6C),
-        Value(0xC0AC29B7C97C50DD),
-        Value(0x3F84D5B5B5470917),
-        Value(0x9216D5D98979FB1B),
+        0x13198A2E03707344,
+        0xA4093822299F31D0,
+        0x082EFA98EC4E6C89,
+        0x452821E638D01377,
+        0xBE5466CF34E90C6C,
+        0xC0AC29B7C97C50DD,
+        0x3F84D5B5B5470917,
+        0x9216D5D98979FB1B,
     ]
 
-    a = Value(0x243F6A8885A308D3)
+    a = 0x243F6A8885A308D3
 
-    def __init__(self, key, tweak, rounds):
-        self.T = Value(tweak)
+    def __init__(
+        self, k0: int, k0prime: int, k1: int, rounds: int, tweak: int, message: int
+    ):
+        self.k0 = k0
+        self.k0prime = k0prime
+        self.k1 = k1
         self.rounds = rounds
+        self.T = tweak
+        self.IS = message
 
-        self.k0 = Value(key[:8])
-        self.k1 = Value(key[8:])
-        self.k0prime = self.k0.ror(1) ^ self.k0 >> 63
-
-    def encrypt(self, plaintext):
-        self.IS = Value(plaintext)
+    def run(self):
         self.add_tweakey(self.k0 ^ self.k1 ^ self.T)
 
         for r in range(self.rounds):
@@ -119,24 +77,16 @@ class Mantis:
             self.round_inverse(r)
 
         self.add_tweakey(self.k0prime ^ self.k1 ^ self.a ^ self.T)
-        return self.IS.to_bytes()
+        return self.IS
 
-    def decrypt(self, ciphertext):
-        old_values = (self.k0, self.k0prime, self.k1)
-        try:
-            (self.k0, self.k0prime, self.k1) = (self.k0prime, self.k0, self.k1 ^ self.a)
-            return self.encrypt(ciphertext)
-        finally:
-            (self.k0, self.k0prime, self.k1) = old_values
-
-    def round(self, r):
+    def round(self, r: int):
         self.sub_cells()
         self.add_constant(r)
         self.add_round_tweakey()
         self.permutate_cells()
         self.mix_columns()
 
-    def round_inverse(self, r):
+    def round_inverse(self, r: int):
         self.mix_columns()
         self.permutate_cells_inverse()
         self.add_round_tweakey_inverse()
@@ -162,35 +112,88 @@ class Mantis:
             0x4,
             0x6,
         ]
-        self.IS = self.IS.sbox(sbox)
+        val = self.IS
+        result = 0
 
-    def add_constant(self, c):
+        for i in range(16):
+            result <<= 4
+            nibble = (val & 0xF000000000000000) >> 60
+            result |= sbox[nibble]
+            val <<= 4
+
+        self.IS = result
+
+    def add_constant(self, c: int):
         rc = self.rc[c]
         self.IS ^= rc
 
     def add_round_tweakey(self):
         h = [6, 5, 14, 15, 0, 1, 2, 3, 7, 12, 13, 4, 8, 9, 10, 11]
-        self.T = self.T.permutate(h)
+        self.T = permutate(self.T, h)
         self.IS ^= self.T ^ self.k1
 
     def add_round_tweakey_inverse(self):
         self.IS ^= self.T ^ self.k1 ^ self.a
         h = [4, 5, 6, 7, 11, 1, 0, 8, 12, 13, 14, 15, 9, 10, 2, 3]
-        self.T = self.T.permutate(h)
+        self.T = permutate(self.T, h)
 
-    def add_tweakey(self, tk):
+    def add_tweakey(self, tk: int):
         self.IS ^= tk
 
     def permutate_cells(self):
         P = [0, 11, 6, 13, 10, 1, 12, 7, 5, 14, 3, 8, 15, 4, 9, 2]
-        self.IS = self.IS.permutate(P)
+        self.IS = permutate(self.IS, P)
 
     def permutate_cells_inverse(self):
         P = [0, 5, 15, 10, 13, 8, 2, 7, 11, 14, 4, 1, 6, 3, 9, 12]
-        self.IS = self.IS.permutate(P)
+        self.IS = permutate(self.IS, P)
 
     def mix_columns(self):
-        self.IS = self.IS.mix_columns()
+        def M(v):
+            return [
+                v[1] ^ v[2] ^ v[3],
+                v[0] ^ v[2] ^ v[3],
+                v[0] ^ v[1] ^ v[3],
+                v[0] ^ v[1] ^ v[2],
+            ]
+
+        rows = [
+            (self.IS >> 48) & 0xFFFF,
+            (self.IS >> 32) & 0xFFFF,
+            (self.IS >> 16) & 0xFFFF,
+            (self.IS >> 0) & 0xFFFF,
+        ]
+        rows = M(rows)
+        self.IS = (rows[0] << 48) | (rows[1] << 32) | (rows[2] << 16) | rows[3]
+
+
+class Mantis:
+    a = 0x243F6A8885A308D3
+
+    def __init__(self, key, tweak, rounds: int):
+        self.tweak = to_int(tweak)
+        self.rounds = rounds
+
+        self.k0 = to_int(key[:8])
+        self.k1 = to_int(key[8:])
+        self.k0prime = ror(self.k0, 1) ^ self.k0 >> 63
+
+    def encrypt(self, plaintext):
+        e = Encryption(
+            self.k0, self.k0prime, self.k1, self.rounds, self.tweak, to_int(plaintext)
+        )
+        return to_bytes(e.run())
+
+    def decrypt(self, ciphertext):
+        d = Encryption(
+            self.k0prime,
+            self.k0,
+            self.k1 ^ self.a,
+            self.rounds,
+            self.tweak,
+            to_int(ciphertext),
+        )
+        return to_bytes(d.run())
 
 
 if __name__ == "__main__":
--- python/test_mantis.py
+++ python/test_mantis.py
@@ -1,53 +1,12 @@
 import unittest
 import secrets
-from mantis import Value, Mantis
-
-class TestValue(unittest.TestCase):
-    def test_xor(self):
-        v1 = Value(0x123456789abcdef0)
-        v2 = Value(0x0000010000100010)
-
-        result = v1 ^ v2
-        self.assertEqual(int(result), 0x123457789aacdee0)
-    
-    def test_bytes(self):
-        v1 = Value(bytes.fromhex("123456789abcdef0"))
-        v2 = Value(0x123456789abcdef0)
-        self.assertEqual(v1, v2)
-
-    def test_ror(self):
-        v = Value(0x123456789abcdef3)
-        v = v.ror(4)
-        self.assertEqual(v, Value(0x3123456789abcdef))
-
-    def test_shift(self):
-        v = Value(0x123456789abcdef3)
-        v = v >> 4
-        self.assertEqual(v, Value(0x123456789abcdef))
+from mantis import Mantis
 
 
 class TestMantis(unittest.TestCase):
     key = bytes.fromhex("92f09952c625e3e9d7a060f714c0292b")
     tweak = bytes.fromhex("ba912e6f1055fed2")
     plaintext = bytes.fromhex("3b5c77a4921f9718")
-
-    def test_permutate_cells_reflexive(self):
-        m = Mantis(self.key, self.tweak, 5)
-        m.encrypt(self.plaintext)
-
-        state = m.IS.clone()
-        m.permutate_cells()
-        m.permutate_cells_inverse()
-        self.assertEqual(state, m.IS)
-
-    def test_add_round_tweakey_reflexive(self):
-        m = Mantis(self.key, self.tweak, 5)
-        m.encrypt(self.plaintext)
-
-        tweak = m.T.clone()
-        m.add_round_tweakey()
-        m.add_round_tweakey_inverse()
-        self.assertEqual(tweak, m.T)
 
     def test_vectors_encrypt(self):
         data = [
