5aceb889c092deecb6f6496bdfcffbaa376b6065
Sjoerd Langkemper <sjoerd-github@linuxonly.nl>
2023-11-03 16:27:54 +0100

    Add fixed decode test

--- python/test_base58.py
+++ python/test_base58.py
@@ -10,6 +10,13 @@ class TestBase58(unittest.TestCase):
         self.assertEqual("2121BWoLqHW", encode(0x05FAFAFAFAFAFAF5))
         self.assertEqual("jpXCZedGfVQ", encode(0xFFFFFFFFFFFFFFFF))
 
+    def test_decode_fixed(self):
+        self.assertEqual(0, decode("11111111111"))
+        self.assertEqual(1, decode("11111111112"))
+        self.assertEqual(58, decode("11111111121"))
+        self.assertEqual(0x05FAFAFAFAFAFAF5, decode("2121BWoLqHW"))
+        self.assertEqual(0xFFFFFFFFFFFFFFFF, decode("jpXCZedGfVQ"))
+
     def test_encode_decode(self):
         for i in range(0, 0xFFFFFFFFFFFFFFFF, 0x1010101010101):
             self.assertEqual(i, decode(encode(i)))
