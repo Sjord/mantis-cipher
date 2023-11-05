78459b20bb7643e650de78598600f28d089e92a8
Sjoerd Langkemper <sjoerd-github@linuxonly.nl>
2023-11-05 13:44:52 +0100

    Add input validation to base58 functions

--- python/base58.py
+++ python/base58.py
@@ -1,17 +1,31 @@
 alphabet = "123456789ABCDEFGHJKLMNPQRSTUVWXYZabcdefghijkmnopqrstuvwxyz"
 
+max_int = 0xFFFFFFFFFFFFFFFF
+max_token = "jpXCZedGfVQ"
+
 
 def encode(input: int) -> str:
+    if input < 0 or input > max_int:
+        raise ValueError(f"Expected integer between 0 and {max_int}, got {input}")
+
     result = ""
     num = input
     for i in range(11):
         num, rem = divmod(num, 58)
         result = alphabet[rem] + result
+    assert num == 0
     return result
 
 
 def decode(input: str) -> int:
-    assert len(input) == 11
+    if len(input) != 11:
+        raise ValueError(
+            f"Expected token of length 11, got token of length {len(input)}"
+        )
+
+    if input > max_token:
+        raise ValueError("Token is too big to decode into a 64-bit integer")
+
     result = 0
     for ch in input:
         result *= 58
--- python/test_base58.py
+++ python/test_base58.py
@@ -1,5 +1,5 @@
 import unittest
-from base58 import encode, decode
+from base58 import *
 
 
 class TestBase58(unittest.TestCase):
@@ -10,6 +10,12 @@ class TestBase58(unittest.TestCase):
         self.assertEqual("2121BWoLqHW", encode(0x05FAFAFAFAFAFAF5))
         self.assertEqual("jpXCZedGfVQ", encode(0xFFFFFFFFFFFFFFFF))
 
+        with self.assertRaises(ValueError):
+            encode(-1)
+
+        with self.assertRaises(ValueError):
+            encode(0x10000000000000000)
+
     def test_decode_fixed(self):
         self.assertEqual(0, decode("11111111111"))
         self.assertEqual(1, decode("11111111112"))
@@ -17,6 +23,22 @@ class TestBase58(unittest.TestCase):
         self.assertEqual(0x05FAFAFAFAFAFAF5, decode("2121BWoLqHW"))
         self.assertEqual(0xFFFFFFFFFFFFFFFF, decode("jpXCZedGfVQ"))
 
+        with self.assertRaises(ValueError):
+            decode("123456789a")
+
+        with self.assertRaises(ValueError):
+            decode("jpXCZedGfVR")
+
+        with self.assertRaises(ValueError):
+            decode("zzzzzzzzzzz")
+
+        with self.assertRaises(ValueError):
+            decode("11111111!!!")
+
     def test_encode_decode(self):
         for i in range(0, 0xFFFFFFFFFFFFFFFF, 0x1010101010101):
             self.assertEqual(i, decode(encode(i)))
+
+    def test_max_int_token(self):
+        self.assertEqual(encode(max_int), max_token)
+        self.assertEqual(decode(max_token), max_int)
